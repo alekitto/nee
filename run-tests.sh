9264c0bc2460154aa8919f9d939a852cb9025b57
Alessandro Chitolina <alekitto@gmail.com>
2016-08-13 11:51:35 +0200

    improved test script

--- run-tests.sh
+++ run-tests.sh
@@ -3,12 +3,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SUCCESS=true
 
-for f in $DIR/tests/*.js ; do
+for f in $DIR/tests/test-*.js ; do
     if [ -f "$f" ] ; then
-        node "$f"
+        printf "Executing $f... "
+        OUTPUT=$(node "$f" 2>&1)
 
         if [ $? -ne 0 ] ; then
+            echo -e "\xC3\x97"
             SUCCESS=false
+
+            echo
+            echo "$OUTPUT"
+            echo
+        else
+            echo -e "\xE2\x9C\x93"
         fi
     fi
 done
